fb0be20758ad62aac1383b6998fa2fb26145fde5
Mas Tsabio <prinztsabioprabhudanishwara@gmail.com>
2025-11-19 10:55:08 +0700

    web 100% ready

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+//for (let i=1; i < 5; i++) {
+    //console.log('hai bang')
+//}
+
+const fetchApi = async () => {
+    try{
+        const res = await fetch ("https://dummyjson.com/quotes");
+        const data = await res.json();
+
+        const container = document.getElementById("container");
+
+
+        data.quotes.map((item) =>{
+            const p = document.createElement("p");
+            p.textContent = `${item.quote} ${item.author}`;
+            container.appendChild(p);
+
+        });
+    }
+    catch (error) {
+        console.log("Error:", error)
+    }
+}
+
+fetchApi()
+
+
+
+const nama = ['Verstappen', 'Norris', 'Russel', 123];
+
+nama.map((item) => {
+    console.log(item);
+})
